b7685cd2b2f218f9f4ef90c91b581c18846be0b5
Srlion <srlion32@gmail.com>
2024-10-27 18:37:04 +0300

    first commit

---
+++ lua/autorun/net_logger.lua
@@ -0,0 +1,54 @@
+originalSendToServer = originalSendToServer or net.SendToServer
+originalSend = originalSend or net.Send
+originalBroadcast = originalBroadcast or net.Broadcast
+originalReceive = originalReceive or net.Receive
+originalStart = originalStart or net.Start
+
+local current_message_name = "none"
+function net.Start(message_name, ...)
+    current_message_name = message_name
+    return originalStart(message_name, ...)
+end
+
+if CLIENT then
+    function net.SendToServer()
+        local bytes = net.BytesWritten()
+
+        print(string.format("[NetLogger] Sent to server: %s (%d bytes)", current_message_name, bytes))
+
+        return originalSendToServer()
+    end
+end
+
+if SERVER then
+    function net.Send(ply)
+        local bytes = net.BytesWritten()
+        local target = IsEntity(ply) and ply:Nick() or "Multiple Players"
+
+        print(string.format("[NetLogger] Sent to %s: %s (%d bytes)", target, current_message_name, bytes))
+
+        return originalSend(ply)
+    end
+
+    function net.Broadcast()
+        local bytes = net.BytesWritten()
+
+        print(string.format("[NetLogger] Broadcast: %s (%d bytes)", current_message_name, bytes))
+
+        return originalBroadcast()
+    end
+end
+
+function net.Receive(message_name, callback)
+    local wrapped_callback
+    if callback then
+        wrapped_callback = function(len, ply)
+            local sender = SERVER and (IsValid(ply) and ply:Nick() or "Unknown") or "Server"
+            print(string.format("[NetLogger] Received from %s: %s (%d bytes)", sender, message_name, len or 0))
+
+            return callback(len, ply)
+        end
+    end
+
+    return originalReceive(message_name, wrapped_callback or callback)
+end
